0660219210d3297c4d28b04f3b56f4e64fbf0daf
abhich21 <96258754+abhich21@users.noreply.github.com>
2022-03-09 19:12:27 +0530

    assignment done

--- Unit-5/sprint-2/day5-useeffect1/src/components/Todos.jsx
+++ Unit-5/sprint-2/day5-useeffect1/src/components/Todos.jsx
@@ -1,8 +1,22 @@
 import { useEffect, useState } from "react"
 
+import axios from "axios";
+
 
 export const Todos = () => {
     const [task, setTask] = useState("");
+
+    const [list, setList] = useState([]);
+
+    useEffect(() => {
+        getTasks();
+    }, []);
+    
+    const getTasks = () => {
+         axios.get("http://localhost:3001/tasks").then((res) => {
+            setList(res.data)
+        })
+    }
     
     return <div>
         <input type="text" onChange={(e) => setTask(e.target.value)} />
@@ -14,8 +28,13 @@ export const Todos = () => {
                     headers: {
                         "Content-Type" : "application/json",
                     },
+                }).then(() => {
+                    getTasks();
                 });
         }}
         >Save</button>
+       { list.map((e)=>(
+           <h3 key={e.id}>{ e.title}</h3>
+        ))}
     </div>
 }
